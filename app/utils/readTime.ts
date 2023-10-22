248d09573e08708c19086df5d3efe555256e2396
Luke Colvin <97006355+lucolvin@users.noreply.github.com>
2023-10-22 14:36:37 -0500

    new commit
    New

--- app/utils/readTime.ts
+++
@@ -1,4 +0,0 @@
-export function readTime({ words }: { words: string }) {
-  const avgReadTime: number = 185;
-  return `${(words.length / avgReadTime).toFixed(0)} min`;
-}
--- schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import job from "./job";
-import profile from "./profile";
-import project from "./project";
-import post from "./post";
-import author from "./author";
-import heroe from "./heroe";
-
-export const schemaTypes = [profile, job, project, post, author, heroe];
